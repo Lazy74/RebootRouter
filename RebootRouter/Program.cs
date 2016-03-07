28dcc379038384d797f5337c79ed36f4c71cff4e
KasyanenkoID <lazy-777@yandex.ru>
2016-03-07 14:42:05 +0600

    Чет не работает пинг =(

--- RebootRouter/Program.cs
+++ RebootRouter/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 
@@ -8,13 +10,36 @@ class TcpClientSample
 {
     public static void Main()
     {
-        Reboot();
+        Console.Write(MyPing("ya.ru"));
+        //Reboot();
+        Console.ReadKey(true);
+    }
+
+    static int MyPing(string addr)
+    {
+        Ping ping = new Ping();
+        PingReply pingReply = null;
+        try
+        {
+            pingReply = ping.Send(addr, 10);
+        }
+        catch (Exception)
+        {
+            pingReply = null;
+        }
+        if (pingReply != null && pingReply.Status == IPStatus.Success)
+        {
+            return Convert.ToInt32(pingReply.RoundtripTime); //время ответа
+        }
+        else
+        {
+            return 0;
+        }
     }
 
     static void Reboot()
     {
         byte[] data = new byte[1024];
-        string input, stringData;
         TcpClient server;
         try
         {
@@ -22,22 +47,12 @@ class TcpClientSample
         }
         catch (SocketException)
         {
-            //Console.WriteLine("Unable to connect to server");
             return;
         }
         Thread.Sleep(1000);
-
-        //NetworkStream ns = server.GetStream();
-        //int recv = ns.Read(data, 0, data.Length);
-        //stringData = Encoding.ASCII.GetString(data, 0, recv);
-        //Console.WriteLine(stringData);
         SendCmd(server, "admin");
         SendCmd(server, "pass");
         SendCmd(server, "reboot");
-        //Console.WriteLine("Disconnecting from server...");
-        //Thread.Sleep(1000);
-
-        //ns.Close();
         server.Close();
     }
 
